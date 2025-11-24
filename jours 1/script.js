a719a7d63fd650e5b5c2171ad69b1a9e349081b7
Laura Buaillon <laura.buaillon1@gmail.com>
2025-11-24 20:26:27 +0100

    exercice 8

---
+++ jours 1/script.js
@@ -0,0 +1,79 @@
+/**Exercice 1 **/
+
+const name ="Martin"
+
+let annéeNaissance =2000;
+let annéeActuelle = 2025;
+let total = annéeActuelle - annéeNaissance;
+
+console.log (("Bonjour Mr"), (name), ("vous avez"), (total));
+
+/**Exercice2 **/
+
+let maths = 15;
+let français= 12;
+let hg = 9;
+
+console.log (maths + français + hg /3)
+
+/**Exercice3 **/
+
+let sexe = "femme";
+
+if (sexe === "femme") {
+    console.log ("Cette personne est une femme");
+}else {
+    console.log ("Cette personne est un homme");
+}
+
+/**Exercice4 **/
+
+let hour = 22;
+
+if (hour >=6 && hour <12) {
+    console.log ("c'est la matin");
+}else if (hour >= 12 && hour <20) {
+    console.log ("c'est l'après midi");
+}else {
+    console.log ("c'est la nuit");
+}
+
+/**Exercice5 **/
+
+let choix = 2;
+
+if (choix === 1){
+    console.log ("Insérer");
+}else if (choix === 2){
+    console.log ("Supprimer");
+}else if (choix === 3){
+    console.log ("Afficher")
+}else {
+    console.log ("Ce choix n'existe pas")
+}
+
+/**Exercice6**/
+
+console.log(15 % 3);
+console.log(15 % 5);
+
+
+/**Exercice7**/
+
+
+/**Exercice 8**/
+
+let cheval = 20;
+
+if (cheval >=0 && cheval<19){
+    console.log ("afficher")
+}else {
+    console.log ("ne pas afficher")
+}
+
+
+
+
+
+
+
